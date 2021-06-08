a0dfa37487d8fd09b472bfd4746286f3c6b090c0
Juan Pablo <juan.saucedo@mi.unc.edu.ar>
2021-06-07 21:14:05 -0300

    Added authentication

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 # Cargar configuración para el proxy reverso
 sudo cp ./nginx/sites-available-proxy /etc/nginx/sites-available/
 sudo nginx -s reload
+
+# copiar proxy en /etc/nginx/sites-available/default
+# copiar .htpasswd en /etc/apache2
